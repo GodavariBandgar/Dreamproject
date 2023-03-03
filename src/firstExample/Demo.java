84a0c58214b01cb7140787b483c6f65e0243792a
Godavari Bandgar <bandgar 101@gmail.com>
2023-03-03 16:17:47 +0530

    changes by goda

---
+++ src/firstExample/Demo.java
@@ -0,0 +1,8 @@
+package firstExample;
+
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("Welcome to bikkad It");
+	}
+
+}
---
+++ src/secondexample/Demo1.java
@@ -0,0 +1,8 @@
+package secondexample;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		System.out.println("I want to become java Developer");
+	}
+}
